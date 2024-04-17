db15f0b2085556c17e2fdd28bf0c7a0d93277bba
CongInCA <captainchenn@163.com>
2024-04-17 11:46:45 -0500

    Add validation to order_item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,13 @@
 class OrderItem < ApplicationRecord
     belongs_to :order
     belongs_to :product
+        
+    validates :order_id, :product_id, presence: true
+
+    validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
     def self.ransackable_associations(auth_object = nil)
         ["order", "product"]
       end
