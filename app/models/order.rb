44f6e7c957d4c789a89326b12199ee9260602e56
CongInCA <captainchenn@163.com>
2024-04-17 11:42:32 -0500

    Add validation to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,10 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :order_items
 
+    validates :user_id, presence: true
+
+    validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
     def self.ransackable_associations(auth_object = nil)
         ["order_items", "user"]
       end
