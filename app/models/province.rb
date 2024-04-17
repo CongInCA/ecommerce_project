606e7d16542985f5ef23817027f58221b987092a
CongInCA <captainchenn@163.com>
2024-04-17 11:43:16 -0500

    Add validation to province

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,8 @@
 class Province < ApplicationRecord
     has_many :users
+
+    validates :name, presence: true
+
     def self.ransackable_attributes(auth_object = nil)
         ["created_at", "gst_rate", "hst_rate", "id", "id_value", "name", "pst_rate", "updated_at"]
       end
